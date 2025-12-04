f52cc2f6d2d277d2cfce2be55cb6e4293890adf0
Md Sakibul Hussain (GROUTINE) <sakal@groutine.pc>
2025-12-04 16:31:10 +0530

    Two

--- build.sh
+++ build.sh
@@ -3,7 +3,12 @@
 set -o errexit
 
 pip install -r requirements.txt
-python3 manage.py migrate --no-input
 
 # CRITICAL: Collect all static files (CSS, JS, images)
 python3 manage.py collectstatic --no-input
+
+python manage.py migrate --noinput && gunicorn BF.wsgi
+
+python manage.py migrate --noinput && \
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('sakal', 'sakalytshit@gmail.com', 'Salibill1')" | python manage.py shell && \
+gunicorn YOUR_PROJECT_NAME.wsgi
