8393280670591b0779e73bc408e285dc9a488149
Md Sakibul Hussain (GROUTINE) <sakal@groutine.pc>
2025-12-21 17:07:06 +0530

    Supabase superuser

--- build.sh
+++ build.sh
@@ -11,13 +11,13 @@ python3 manage.py collectstatic --no-input
 # Run migrations
 python3 manage.py migrate --noinput
 
-# Create superuser ONLY if it doesn't exist
-# python3 manage.py shell << EOF
-# from django.contrib.auth import get_user_model
-# User = get_user_model()
-# if not User.objects.filter(username='sakal').exists():
-#     User.objects.create_superuser('sakal', 'sakalytshit@gmail.com', 'Salibill1')
-#     print('✅ Superuser created')
-# else:
-#     print('ℹ️ Superuser already exists')
-# EOF
+Create superuser ONLY if it doesn't exist
+python3 manage.py shell << EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='sakal').exists():
+    User.objects.create_superuser('sakal', 'sakalytshit@gmail.com', 'Salibill1')
+    print('✅ Superuser created')
+else:
+    print('ℹ️ Superuser already exists')
+EOF
