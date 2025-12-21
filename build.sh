f69e33fb56a2a6ed62e1a09ac37edcdccb07105d
Md Sakibul Hussain (GROUTINE) <sakal@groutine.pc>
2025-12-21 12:22:19 +0530

    Railway fix 2

--- build.sh
+++ build.sh
@@ -1,23 +1,26 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
+# #!/usr/bin/env bash
+# # exit on error
+# set -o errexit
 
-# Install dependencies
-pip install -r requirements.txt
+# # Install dependencies
+# pip install -r requirements.txt
 
-# Collect static files
-python3 manage.py collectstatic --no-input
+# # Collect static files
+# python3 manage.py collectstatic --no-input
 
-# Run migrations
-python3 manage.py migrate --noinput
+# # Run migrations
+# python3 manage.py migrate --noinput
 
-# Create superuser ONLY if it doesn't exist
-# python3 manage.py shell << EOF
-# from django.contrib.auth import get_user_model
-# User = get_user_model()
-# if not User.objects.filter(username='sakal').exists():
-#     User.objects.create_superuser('sakal', 'sakalytshit@gmail.com', 'Salibill1')
-#     print('✅ Superuser created')
-# else:
-#     print('ℹ️ Superuser already exists')
-# EOF
+# # Create superuser ONLY if it doesn't exist
+# # python3 manage.py shell << EOF
+# # from django.contrib.auth import get_user_model
+# # User = get_user_model()
+# # if not User.objects.filter(username='sakal').exists():
+# #     User.objects.create_superuser('sakal', 'sakalytshit@gmail.com', 'Salibill1')
+# #     print('✅ Superuser created')
+# # else:
+# #     print('ℹ️ Superuser already exists')
+# # EOF
+
+#!/usr/bin/env sh
+echo "Skipping build step on Railway"
